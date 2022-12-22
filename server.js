71f3d5d244c6af3dbc0b0525d5167852882c76e9
VikashK21 <vikash21@navgurukul.org>
2022-12-22 15:31:58 +0530

    the changes

--- server.js
+++ server.js
@@ -20,17 +20,17 @@ const PORT = process.env.PORT || 8888;
 //https://expressjs.com/en/guide/writing-middleware.html
 // console.log(path.join(__dirname, "../client/build/index.html"));
 app.use(cors());
-// app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 // app.use(express.static(path.join(__dirname, "../client/public")));
 // app.use(express.static())
-app.get("/meetclub", (req, res, next) => {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+app.get("/", (req, res, next) => {
+  // app.use(express.static(path.join(__dirname, "/client/build")));
   //default room
   // /home/vikash/Desktop/BASK/bb_video_call/client/build/index.html
   res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
-app.get("/meetclub/:room", (req, res, next) => {
+app.get("/:room", (req, res, next) => {
   res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
