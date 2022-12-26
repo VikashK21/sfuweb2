0a886003a0f6a77962c2d86ddbc1a7ae323c2cd7
VikashK21 <vikash21@navgurukul.org>
2022-12-26 14:08:33 +0530

    the change

--- client/src/App.js
+++ client/src/App.js
@@ -419,27 +419,28 @@ function App() {
   const getLocalStream = () => {
     navigator.mediaDevices
       .getUserMedia({
-        // audio: true,
-        // video: {
-        //   width: {
-        //     min: 640,
-        //     max: 1920,
-        //   },
-        //   height: {
-        //     min: 400,
-        //     max: 1080,
-        //   },
-        // },
-        audio: {},
+        audio: true,
         video: {
-          deviceId: { exact: "id of logitech camera" },
-          frameRate: { ideal: 30 },
-          height: { ideal: 720 }, // drop down to 720p
-          width: { ideal: 1280 }, // drop down to 1280p
+          width: {
+            min: 640,
+            max: 1920,
+          },
+          height: {
+            min: 400,
+            max: 1080,
+          },
         },
+        // audio: {},
+        // video: {
+        //   deviceId: { exact: "id of logitech camera" },
+        //   frameRate: { ideal: 30 },
+        //   height: { ideal: 720 }, // drop down to 720p
+        //   width: { ideal: 1280 }, // drop down to 1280p
+        // },
       })
       .then(streamSuccess)
       .catch((err) => console.log(err.message));
+    
   };
 
   const mutemic = (e) => {
