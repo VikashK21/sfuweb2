0944266de104ca56ea5ce7c1f9f8eb89e9a5e741
VikashK21 <vikash21@navgurukul.org>
2022-12-26 12:52:44 +0530

    the change

--- server.js
+++ server.js
@@ -236,14 +236,14 @@ connection.on("connection", async (socket) => {
       try {
         const webRTcTransport_options = {
           listenIps: [
-            // {
-            //   ip: "0.0.0.0",
-            //   announcedIp: getLocalIp(), // replace by public IP address.
-            // },
             {
               ip: "0.0.0.0",
-              announcedIp: "52.87.191.26",
+              announcedIp: getLocalIp(), // replace by public IP address.
             },
+            // {
+            //   ip: "0.0.0.0",
+            //   announcedIp: "52.87.191.26",
+            // },
           ],
           // listenIps: getLocalIp(),
           initialAvailableOutgoingBitrate: 1000000,
